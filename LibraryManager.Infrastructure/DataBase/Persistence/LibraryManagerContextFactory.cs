8e14bdf0ea09f9862c936dee349b653097731f22
Math Peeters <pmathieu.peeters@gmail.com>
2026-02-26 00:24:09 +0100

    context factory pas fini

---
+++ LibraryManager.Infrastructure/DataBase/Persistence/LibraryManagerContextFactory.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore.Design;
+
+namespace LibraryManager.Infrastructure.DataBase.Persistence;
+
+public class LibraryManagerContextFactory : IDesignTimeDbContextFactory<LibraryManagerContext>
+{
+  public LibraryManagerContext CreateDbContext(string[] args)
+  {
+    var apiPath = Path.Combine(Directory.GetCurrentDirectory(), "../LibraryManager.Api");
+  }
+}
