1e27b10ffc7667b48feaac4753575d8ad9a084e0
Math Peeters <pmathieu.peeters@gmail.com>
2026-03-08 00:17:28 +0100

    data annotation LoginRequestDto.cs

--- LibraryManager.Core/DTOs/Requests/UserRequest/LoginRequestDto.cs
+++ LibraryManager.Core/DTOs/Requests/UserRequest/LoginRequestDto.cs
@@ -1,7 +1,22 @@
-﻿namespace LibraryManager.Core.DTOs.Requests.UserRequest;
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace LibraryManager.Core.DTOs.Requests.UserRequest;
 
 public class LoginRequestDto
 {
+  [Required(ErrorMessage = "L'email est requis")]
+  [RegularExpression(@"^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$", 
+    ErrorMessage = "Le format de l'email n'est pas valide")]
+  [MaxLength(45, ErrorMessage = "Max Length is 45")]
+  [Display(Name = "Adresse Email")]
   public required string Email { get; set; } = string.Empty;
+  
+  [Required(ErrorMessage = "Password is required")]
+  [RegularExpression(@"^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{6,}$", 
+    ErrorMessage = "Le mot de passe doit contenir une majuscule, une minuscule, un chiffre et un caractère spécial.")]
+  [MinLength(6, ErrorMessage = "Min Length is 6")]
+  [MaxLength(20, ErrorMessage = "Max Length is 20")]
+  [Display(Name = "Mot de passe")]
+  [DataType(DataType.Password)]
   public required string Password { get; set; } =  string.Empty;
 }
