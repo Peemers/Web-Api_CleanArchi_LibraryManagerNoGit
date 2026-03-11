7735ee86a7924a81406280867fcc20ed552f3550
Math Peeters <pmathieu.peeters@gmail.com>
2026-03-11 12:23:30 +0100

    premiere imp de serilogger

--- LibraryManager.API/Program.cs
+++ LibraryManager.API/Program.cs
@@ -9,77 +9,95 @@ using LibraryManager.Core.Services.Data;
 using LibraryManager.Core.Services.Tools;
 using LibraryManager.Infrastructure.Repositories;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
-using Microsoft.IdentityModel.Tokens; // Vérifie que LivreService est bien ici
+using Microsoft.IdentityModel.Tokens;
+using Serilog;
+using Serilog.Core;
+using Serilog.Events; // Vérifie que LivreService est bien ici
 
-var builder = WebApplication.CreateBuilder(args);
 
-// 1. Base de données
-builder.Services.AddDbContext<LibraryManagerContext>(options =>
-  options.UseSqlServer(builder.Configuration.GetConnectionString("Default")));
+Log.Logger = new LoggerConfiguration() //creation du loger avant tout le reste
+  .MinimumLevel.Override("Microsoft", LogEventLevel.Information) //le niveau d'info
+  .Enrich.FromLogContext() //coontexte
+  .WriteTo.Console() //ou ça va etre ecrit
+  .CreateBootstrapLogger();
+
+try
+{
+  var builder = WebApplication.CreateBuilder(args);
+
+//Base de données
+  builder.Services.AddDbContext<LibraryManagerContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("Default")));
 
 //Injection de dépendances
 
-builder.Services.AddScoped<ILivreRepository, LivreRepository>();
-builder.Services.AddScoped<ILivreService, LivreService>();
-builder.Services.AddScoped<IEmpruntRepository, EmpruntRepository>();
-builder.Services.AddScoped<IEmpruntService, EmpruntService>();
-builder.Services.AddScoped<IUserRepository, UserRepository>();
-builder.Services.AddScoped<IUserService, UserService>();
+  builder.Services.AddScoped<ILivreRepository, LivreRepository>();
+  builder.Services.AddScoped<ILivreService, LivreService>();
+  builder.Services.AddScoped<IEmpruntRepository, EmpruntRepository>();
+  builder.Services.AddScoped<IEmpruntService, EmpruntService>();
+  builder.Services.AddScoped<IUserRepository, UserRepository>();
+  builder.Services.AddScoped<IUserService, UserService>();
 
-var allowedOrigins = builder.Configuration.GetSection("AllowedOrigins").Get<string[]>();
-Console.WriteLine($"CORS configurés pour : {string.Join(", ", allowedOrigins ?? [])}");
+  var allowedOrigins = builder.Configuration.GetSection("AllowedOrigins").Get<string[]>();
+  Console.WriteLine($"CORS configurés pour : {string.Join(", ", allowedOrigins ?? [])}");
 
-builder.Services.AddCors(options =>
-{
-  options.AddPolicy("AllowAngular", policy =>
+  builder.Services.AddCors(options =>
   {
-    policy.WithOrigins("http://localhost:4200")
-      .AllowAnyMethod()
-      .AllowAnyHeader();
+    options.AddPolicy("AllowAngular", policy =>
+    {
+      policy.WithOrigins("http://localhost:4200")
+        .AllowAnyMethod()
+        .AllowAnyHeader();
+    });
   });
-});
 
 //Services de base
-builder.Services.AddControllers()
-  .AddJsonOptions(options =>
-  {
-    options.JsonSerializerOptions.PropertyNameCaseInsensitive = true;
-  });
-builder.Services.AddOpenApi();
-builder.Services.AddSingleton<IPasswordHasher, PasswordHasher>();
-builder.Services.AddScoped<IJwtProvider, JwtProvider>();
-builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
-  .AddJwtBearer(options =>
-  {
-    options.TokenValidationParameters = new TokenValidationParameters
+  builder.Services.AddControllers()
+    .AddJsonOptions(options => { options.JsonSerializerOptions.PropertyNameCaseInsensitive = true; });
+  builder.Services.AddOpenApi();
+  builder.Services.AddSingleton<IPasswordHasher, PasswordHasher>();
+  builder.Services.AddScoped<IJwtProvider, JwtProvider>();
+  builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+    .AddJwtBearer(options =>
     {
-      ValidateIssuer = true,
-      ValidateAudience = true,
-      ValidateLifetime = true,
-      ValidateIssuerSigningKey = true,
-      ValidIssuer = builder.Configuration["Jwt:Issuer"],
-      ValidAudience = builder.Configuration["Jwt:Audience"],
-      IssuerSigningKey = new SymmetricSecurityKey(
-        Encoding.UTF8.GetBytes(builder.Configuration["Jwt:Key"]!))
-    };
-  });
+      options.TokenValidationParameters = new TokenValidationParameters
+      {
+        ValidateIssuer = true,
+        ValidateAudience = true,
+        ValidateLifetime = true,
+        ValidateIssuerSigningKey = true,
+        ValidIssuer = builder.Configuration["Jwt:Issuer"],
+        ValidAudience = builder.Configuration["Jwt:Audience"],
+        IssuerSigningKey = new SymmetricSecurityKey(
+          Encoding.UTF8.GetBytes(builder.Configuration["Jwt:Key"]!))
+      };
+    });
 
-var app = builder.Build();
+  var app = builder.Build();
 
 //Pipeline HTTP (Middleware)
-if (app.Environment.IsDevelopment())
-{
-  app.MapOpenApi(); //creation json
-  app.MapScalarApiReference();
+  if (app.Environment.IsDevelopment())
+  {
+    app.MapOpenApi(); //creation json
+    app.MapScalarApiReference();
+  }
+
+  app.UseCors("AllowAngular");
+  app.UseHttpsRedirection();
+  app.UseAuthentication();
+  app.UseAuthorization();
+  app.MapControllers();
+  app.MapGet("/test", () => "API OK");
+
+
+
+  app.Run();
 }
+catch (Exception ex)
+{
 
-app.UseCors("AllowAngular");
-app.UseHttpsRedirection();
-app.UseAuthentication();
-app.UseAuthorization();
-app.MapControllers();
-app.MapGet("/test", () => "API OK");
-
-
-
-app.Run();
+}
+finally
+{
+  
+}
